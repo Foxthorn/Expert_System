b01ae0af78a6ba4003eaa894bfffd661c2542b83
WalkingLight <ryan.heukelman@gmail.com>
2017-12-18 12:04:15 +0200

    Add multiple files

--- main.py
+++ main.py
@@ -16,23 +16,28 @@ def update_facts(f, v_list):
     return v_list
 
 
-change = True
-if len(sys.argv) != 2:
+arg = len(sys.argv)
+if arg == 1:
     print ('Usage: python main.py [filename]')
     sys.exit(1)
-val = Validate(sys.argv[1])
-kb = Validate.get_kb(val)
-facts = val.get_facts()
-q = Validate.get_quarries(val)
-val_list = Validate.get_dictionary(val)
-val_list = update_facts(facts, val_list)
-solve = Solve(kb)
-while change is True:
-    change = False
-    val_list = solve.split_solve(val_list)
-    if solve.change is True:
-        change = True
-query = Query(val_list)
-query.facts(facts)
-print "\n",
-query.query(q)
+i = 1
+while arg > i:
+    change = True
+    print "---------- "+sys.argv[i]+" ----------"
+    val = Validate(sys.argv[i])
+    kb = Validate.get_kb(val)
+    facts = val.get_facts()
+    q = Validate.get_quarries(val)
+    val_list = Validate.get_dictionary(val)
+    val_list = update_facts(facts, val_list)
+    solve = Solve(kb)
+    while change is True:
+        change = False
+        val_list = solve.split_solve(val_list)
+        if solve.change is True:
+            change = True
+    query = Query(val_list)
+    query.facts(facts)
+    print "\n",
+    query.query(q)
+    i = i + 1
